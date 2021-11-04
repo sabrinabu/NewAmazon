1e304a4ae21348dd6bf892b55dd762118e61d94d
sabrinabu <sabrinakhaled13@gmail.com>
2021-11-04 18:25:58 +0100

    add HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import Product from './components/Product';
-import data from './data';
+import { BrowserRouter,Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
       <header className="row">
         <div>
@@ -17,17 +20,13 @@ function App() {
           <a href="/signin">Sign In</a>
         </div>
       </header>
-      <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <Product key={product._id} product={product}></Product>
-            ))}
-          </div>
-        </div>
+      <main> 
+      <Route path="/product/:id" component={ProductScreen}></Route>
+      <Route path="/" component={HomeScreen} exact ></Route>
       </main>
       <footer className="row center">All right reserved</footer>
     </div>
+    </BrowserRouter>
   );
 }
 
