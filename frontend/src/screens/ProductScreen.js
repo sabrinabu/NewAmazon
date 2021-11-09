1d138a9e2785718c709ef70ba669225ca0969f3c
sabrinabu <sabrinakhaled13@gmail.com>
2021-11-09 15:09:42 +0100

    product from backend and created Loading,MessageBox component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -42,7 +42,7 @@ export default function ProductScreen(props) {
              <li>
                 <div className="row">
                     <div>status</div>  
-                      <div>{product.countInStock>0?<span className="success">In Stock</span>:(<span className="error">Unavailable</span>)}</div>
+                      <div>{product.countInStock>0?<span className="success">In Stock</span>:(<span className="danger">Unavailable</span>)}</div>
                        </div>  
                      </li>
                      <li>
@@ -51,6 +51,7 @@ export default function ProductScreen(props) {
                  </ul>
              </div>  
            </div> 
+           
          </div>
        </div>
     )
