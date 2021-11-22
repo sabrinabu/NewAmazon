41ad2abce70eeac8fe89f7dbf14b5bdeebf42f77
sabrinabu <sabrinakhaled13@gmail.com>
2021-11-22 11:45:30 +0100

    Implement signinaction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,63 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { BrowserRouter,Route,Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import cartScreen from './screens/cartScreen';
-import signinScreen from './screens/signinScreen';
-import SigninScreen from './screens/signinScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
-  const cart= useSelector(state => state.cart);
-  const {cartItems}=cart;
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <Link className="brand" to="/">
-            amazona
-          </Link>
-        </div>
-        <div>
-          <Link to="/cart">Cart{cartItems.length > 0 && (
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              amazona
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
                 <span className="badge">{cartItems.length}</span>
-              )}</Link>
-          <Link to="/signin">Sign In</Link>
-        </div>
-      </header>
-      <main> 
-      <Route path="/cart/:id?" component={cartScreen}></Route>
-      <Route path="/product/:id" component={ProductScreen}></Route>
-      <Route path="/Signin" component={SigninScreen}></Route>
-      <Route path="/" component={HomeScreen} exact ></Route>
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
     </BrowserRouter>
   );
 }
